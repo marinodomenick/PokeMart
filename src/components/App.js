8a4314d05e286fc1226ab8948bc7034c0a0439e1
DippinDot <jpmarino33@gmail.com>
2022-08-16 14:59:10 -0600

    items single items

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,5 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
-
-// getAPIHealth is defined in our axios-services directory index.js
-// you can think of that directory as a collection of api adapters
-// where each adapter fetches specific info from our express server's /api route
 import { getAPIHealth } from "../axios-services";
 import "../style/App.css";
 import Items from "./Items";
@@ -13,23 +9,15 @@ const App = () => {
   const [APIHealth, setAPIHealth] = useState("");
 
   useEffect(() => {
-    // follow this pattern inside your useEffect calls:
-    // first, create an async function that will wrap your axios service adapter
-    // invoke the adapter, await the response, and set the data
     const getAPIStatus = async () => {
       const { healthy } = await getAPIHealth();
       setAPIHealth(healthy ? "api is up! >:D" : "api is down xD");
     };
-
-    // second, after you've defined your getter above
-    // invoke it immediately after its declaration, inside the useEffect callback
     getAPIStatus();
   }, []);
 
   return (
-    // console.log(items, "Items"),
     <>
-      {/* <div className="app-container"> */}
       <div>
         <h1>Hello, World!</h1>
         <p>API Status: {APIHealth}</p>
