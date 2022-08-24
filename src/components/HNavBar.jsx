2ffd795dfe9e2a02669a166b9369f9eb4b70e6b9
nex-00 <kendaljane10@gmail.com>
2022-08-24 17:06:05 -0600

    we're safe thank god

--- src/components/HNavBar.jsx
+++ src/components/HNavBar.jsx
@@ -24,7 +24,7 @@ const HNavBar = () => {
        
         </div>
         <div className="cart">
-        <Link to="/cart">Cart Link</Link>
+        <Link to="/cart">Cart</Link>
         </div>
     </nav>
   )
